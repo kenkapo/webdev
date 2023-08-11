d9221b647ee3ed48108ceca8ab2704e8e3ebb498
kenkapo <58553879+kenkapo@users.noreply.github.com>
2023-08-11 18:41:11 +0530

    Create backend.js

---
+++ backend.js
@@ -0,0 +1,4 @@
+API->
+1. Endpoints->there are multiple endpoints in an API and they are used for different purposes;
+2. Query parametrs->Used for filtering and searching and sorting;
+3. Path parameters->they are used to get a specific resource only. Every data has some unique property and we use these params. to get that specific data.
